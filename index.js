2add96d941f3e98516ff6518adb15b7405fd4312
Ziyad Bhombal <zeebhombal@gmail.com>
2022-11-24 00:58:06 +0530

    feat: cleanup

--- index.js
+++ index.js
@@ -1,60 +1,16 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const markdownTable = require("markdown-table");
 const fs = require("fs");
 const path = require("path");
 
-function getExamples(results) {
-  return getChildren(results, []);
-}
-
-function getChildren(input, output, filepath) {
-  Object.values(input).forEach(({ tests, suites, file }) => {
-    if (file) {
-      filepath = file;
-    }
-
-    if (tests) {
-      tests.forEach(
-        ({ fail, pending, skipped, fullTitle, err: { message } }) => {
-          if (fail || pending || skipped) {
-            output.push({
-              title: fullTitle,
-              filepath,
-              message: message ? message.replace(/\n+/g, " ") : null,
-              state: fail ? "fail" : skipped ? "skipped" : "pending",
-            });
-          }
-        }
-      );
-    }
-
-    if (suites) {
-      output = [...getChildren(suites, output, filepath)];
-    }
-  });
-
-  return output;
-}
-
-function getTable(examples) {
-  return markdownTable([
-    ["State", "Description"],
-    ...examples.map(({ state, filepath, title, message }) => [
-      state,
-      `**Filepath**: ${filepath}<br>**Title**: ${title}<br>**Error**: ${message}`,
-    ]),
-  ]);
-}
-
 function getSummary(stats) {
-  return (
-    `Passes: ${stats.passes},` +
-    ` failures: ${stats.failures},` +
-    ` pending: ${stats.pending},` +
-    ` skipped: ${stats.skipped},` +
-    ` other: ${stats.other}.`
-  );
+  return {
+    passed: stats.passes,
+    failures: stats.failures,
+    pending: stats.pending,
+    skipped: stats.skipped,
+    duration: stats.duration,
+  };
 }
 
 async function run() {
@@ -72,15 +28,18 @@ async function run() {
 
     const result = require(fullPathname);
 
+    const data = getSummary(result.stats);
+
     core.setOutput(
       "commentBody",
-      `${core.getInput("title", { required: true })}
-    <details>
-    <summary>${getSummary(result.stats)}</summary>
-
-    ${getTable(getExamples(result.results))}
-
-    </details>`
+      `
+      ## :evergreen_tree: ${core.getInput("title", { required: true })}</h1>
+      | Passed ✅ | Failed ❌ | Pending ✋🏻 | Skipped ↩️ | Duration ⏱️ |
+      |---|---|---|---|---|
+      | ${data.passed} | ${data.failures} | ${data.pending} | ${
+        data.skipped
+      } | ${data.duration} |
+      `
     );
   } catch (error) {
     core.setFailed(error.message);
